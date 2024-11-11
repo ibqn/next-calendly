bb77d87c807bc408f475ac8d5e863954281741f5
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-11-11 09:18:28 +0100

    setup clerk auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
-export default function Home() {
-  return <div>hello</div>
+import { Button } from "@/components/ui/button"
+import { SignInButton, SignUpButton } from "@clerk/nextjs"
+
+export default function HomePage() {
+  return (
+    <div className="container mx-auto my-4 text-center">
+      <h1 className="mb-4 text-3xl">Home Page</h1>
+
+      <div className="flex justify-center gap-2">
+        <Button asChild>
+          <SignInButton />
+        </Button>
+        <Button asChild>
+          <SignUpButton />
+        </Button>
+      </div>
+    </div>
+  )
 }
