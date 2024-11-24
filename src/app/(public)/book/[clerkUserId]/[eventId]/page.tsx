492da302dcea6f3e1e74eab79b28d3e48e7e4bc0
Evgeny Bobkin <evgeny.bobkin@europace.de>
2024-11-24 14:04:04 +0100

    wip on meeting form

--- src/app/(public)/book/[clerkUserId]/[eventId]/page.tsx
+++ src/app/(public)/book/[clerkUserId]/[eventId]/page.tsx
@@ -1,3 +1,11 @@
+import { MeetingForm } from "@/components/forms/meeting-form"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { db } from "@/drizzle/db"
+import { getValidTimesFromSchedule } from "@/lib/times-from-schedule"
+import { clerkClient } from "@clerk/nextjs/server"
+import { addMonths, eachMinuteOfInterval, endOfDay, roundToNearestMinutes } from "date-fns"
+import { notFound } from "next/navigation"
+
 type Params = Promise<{
   clerkUserId: string
   eventId: string
@@ -7,20 +15,43 @@ type Props = Readonly<{
   params: Params
 }>
 
-function delay(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms))
-}
-
 export default async function BookEventPage({ params }: Props) {
-  const { clerkUserId, eventId } = await params
+  const { clerkUserId: userId, eventId } = await params
 
-  await delay(2000)
+  const event = await db.query.EventTable.findFirst({
+    where: ({ clerkUserId, id, isActive }, { eq, and }) =>
+      and(eq(id, eventId), eq(isActive, true), eq(clerkUserId, userId)),
+  })
+
+  if (!event) {
+    return notFound()
+  }
+
+  const clerk = await clerkClient()
+
+  const calenderUser = await clerk.users.getUser(userId)
+
+  const startDate = roundToNearestMinutes(new Date(), { nearestTo: 15, roundingMethod: "ceil" })
+  const endDate = endOfDay(addMonths(startDate, 2))
+
+  const timesInOrder = eachMinuteOfInterval({ start: startDate, end: endDate }, { step: 15 })
+
+  const validTimes = await getValidTimesFromSchedule(timesInOrder, event)
+
+  // console.log("valid times", validTimes)
 
   return (
-    <div>
-      <h1>
-        BookEventPage {clerkUserId} and {eventId}
-      </h1>
-    </div>
+    <Card className="mx-auto max-w-4xl">
+      <CardHeader>
+        <CardTitle>
+          {event.name} with {calenderUser.fullName}
+        </CardTitle>
+        {event.description && <CardDescription>{event.description}</CardDescription>}
+      </CardHeader>
+
+      <CardContent>
+        <MeetingForm eventId={event.id} userId={userId} validTimes={validTimes} />
+      </CardContent>
+    </Card>
   )
 }
---
+++ src/components/forms/meeting-form.tsx
@@ -0,0 +1,221 @@
+"use client"
+
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+import { meetingFormSchema, MeetingFormSchema } from "@/schema/meeting"
+import { format, isSameDay, startOfDay } from "date-fns"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { formatTimezoneOffset } from "@/lib/format-timezone-offset"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import { cn } from "@/lib/utils"
+import { CalendarIcon } from "lucide-react"
+import { Calendar } from "@/components/ui/calendar"
+import { toZonedTime } from "date-fns-tz"
+import { Textarea } from "@/components/ui/textarea"
+
+type Props = {
+  eventId: string
+  userId: string
+  validTimes: Date[]
+}
+
+export const MeetingForm = ({ eventId, userId, validTimes }: Props) => {
+  const form = useForm<MeetingFormSchema>({
+    defaultValues: {
+      timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+      guestEmail: "",
+      guestName: "",
+      guestNotes: "",
+    },
+    resolver: zodResolver(meetingFormSchema),
+  })
+
+  const timezone = form.watch("timezone")
+  const date = form.watch("date")
+
+  const validTimesInTimezone = validTimes.map((time) => toZonedTime(time, timezone))
+
+  const onSubmit = form.handleSubmit(async (data) => {
+    console.log("form", data)
+
+    // const response = await createMeeting(data)
+
+    // if (response?.type === ActionResponseType.error) {
+    //   form.setError("root.actionEvent", { message: response.message })
+    // }
+
+    // if (response?.type === ActionResponseType.success) {
+    //   redirect("/events")
+    // }
+  })
+
+  return (
+    <Form {...form}>
+      <form onSubmit={onSubmit} className="flex flex-col gap-6">
+        {form.formState.errors.root?.actionEvent && (
+          <FormMessage>{form.formState.errors.root.actionEvent.message}</FormMessage>
+        )}
+
+        <FormField
+          control={form.control}
+          name="timezone"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Timezone</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue />
+                  </SelectTrigger>
+                </FormControl>
+
+                <SelectContent>
+                  {Intl.supportedValuesOf("timeZone").map((timezone) => (
+                    <SelectItem key={timezone} value={timezone}>
+                      {timezone} ({formatTimezoneOffset(timezone)})
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+
+              <FormDescription>The timezone that the meeting will be scheduled in.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <div className="flex flex-col gap-6 sm:flex-row sm:items-center">
+          <FormField
+            control={form.control}
+            name="date"
+            render={({ field }) => (
+              <FormItem className="flex flex-1 flex-col">
+                <FormLabel>Date</FormLabel>
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant={"outline"}
+                        className={cn("w-full pl-3 text-left font-normal", !field.value && "text-muted-foreground")}
+                      >
+                        {field.value ? format(field.value, "PPP") : <span>Pick a date</span>}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={field.onChange}
+                      disabled={(date) => !validTimesInTimezone.some((time) => isSameDay(date, time))}
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+                <FormDescription>Choose a date of your booking.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="startTime"
+            render={({ field }) => (
+              <FormItem className="flex flex-1 flex-col">
+                <FormLabel>Time</FormLabel>
+                <Select
+                  disabled={date == null || timezone == null}
+                  onValueChange={(value) => field.onChange(new Date(Date.parse(value)))}
+                  defaultValue={field.value?.toISOString()}
+                >
+                  <FormControl>
+                    <SelectTrigger>
+                      <SelectValue
+                        className="w-full"
+                        placeholder={
+                          date == null || timezone == null ? "Select a date/timezone first" : "Select a meeting time"
+                        }
+                      />
+                    </SelectTrigger>
+                  </FormControl>
+                  <SelectContent>
+                    {validTimesInTimezone
+                      .filter((time) => isSameDay(time, date))
+                      .map((time) => (
+                        <SelectItem key={time.toISOString()} value={time.toISOString()}>
+                          {format(time, "HH:mm")}
+                        </SelectItem>
+                      ))}
+                  </SelectContent>
+                </Select>
+                <FormDescription>Choose a time of your booking.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <FormField
+          control={form.control}
+          name="guestName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Your Name</FormLabel>
+              <FormControl>
+                <Input {...field} placeholder="Your name..." />
+              </FormControl>
+              <FormDescription>Your name will be shared with the host of the meeting.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="guestEmail"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Your Email</FormLabel>
+              <FormControl>
+                <Input {...field} placeholder="Your mail..." />
+              </FormControl>
+              <FormDescription>
+                Your email will be shared with the host of the meeting for communication purposes.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="guestNotes"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Your Notes</FormLabel>
+              <FormControl>
+                <Textarea {...field} placeholder="Your notes..." className="h-32 resize-none" />
+              </FormControl>
+              <FormDescription>Any additional notes you'd like to share with the host of the meeting.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <div className="flex justify-between gap-2">
+          <Button asChild variant="outline" type="button" className="ml-auto">
+            <Link href={`/book/${userId}`}>Cancel</Link>
+          </Button>
+
+          <Button type="submit">Schedule</Button>
+        </div>
+      </form>
+    </Form>
+  )
+}
